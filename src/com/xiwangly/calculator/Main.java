a240438a785bf36e3f97ce5c1559cf5b0d3cac12
xiwangly2 <1334850101@qq.com>
2023-03-02 15:16:01 +0800

    提交，计算器，支持三角函数，用法math.sin(value)

--- src/com/xiwangly/calculator/Main.java
+++ src/com/xiwangly/calculator/Main.java
@@ -7,11 +7,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("请输入计算式：");
-        String message = myScanner.next();
-//        String message = "3+4*5+2";
-        message = URLEncoder.encode( message, "UTF-8" );
-        System.out.println("计算结果：");
-        String return_message = new Calc(message).toString();
+        while (true) {
+            System.out.println("\n请输入计算式：");
+            String message = myScanner.next();
+            if (message.equals("exit")) {
+                break;
+            }
+            message = URLEncoder.encode(message, "UTF-8");
+            System.out.println("计算结果：");
+            String return_message = new Calc(message).toString();
+        }
     }
 }
