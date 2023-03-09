e36b8544881251fb3aab5a33c1376b6503c6bae0
xiwangly2 <1334850101@qq.com>
2023-03-09 09:36:13 +0800

    提交

--- src/com/xiwangly/work/Work10.java
+++ src/com/xiwangly/work/Work10.java
@@ -1,7 +1,27 @@
 package com.xiwangly.work;
 
 public class Work10 {
-    public static void main(String[] args) {
+    private String name;
+    private String color;
+    private String variety;
 
+    public Work10(String name, String color, String variety) {
+        this.name = name;
+        this.color = color;
+        this.variety = variety;
+    }
+
+    public void eat() {
+        System.out.println("啃骨头");
+    }
+
+    public void run() {
+        System.out.println("叼着骨头跑");
+    }
+
+    public static void main(String[] args) {
+        Work10 wuhuarou = new Work10("五花肉", "棕色", "阿拉斯加");
+        wuhuarou.eat();
+        wuhuarou.run();
     }
 }
