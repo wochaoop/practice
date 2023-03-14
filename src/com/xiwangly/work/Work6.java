b12231c9dd455b245656d6f824154215dea5ae0f
xiwangly2 <1334850101@qq.com>
2023-03-14 09:08:03 +0800

    +1

--- src/com/xiwangly/work/Work6.java
+++ src/com/xiwangly/work/Work6.java
@@ -4,11 +4,6 @@ public class Work6 {
     public static void main(String[] args) {
         int a = -10;
         int absValue = (a >> 31) == 0 ? a : (~a + 1);
-        int i = 0;
-        while (i <= 32) {
-            a = a >> 1;
-            i++;
-        }
         System.out.println(absValue);
     }
 }
