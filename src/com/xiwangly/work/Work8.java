d30845a9f5a1cb7ad179e567e514a18927635633
xiwangly2 <1334850101@qq.com>
2023-03-14 09:08:03 +0800

    +1

--- src/com/xiwangly/work/Work8.java
+++ src/com/xiwangly/work/Work8.java
@@ -2,36 +2,10 @@ package com.xiwangly.work;
 
 public class Work8 {
     public static void main(String[] args) {
-        int num = -10;
-        long absNum = Math.abs((long) num);  // 转换为long类型，确保位运算不出错
-        char[] binaryArray = new char[32];
-        int index = 0;
-
-        while (index < 32) {
-            binaryArray[index] = (absNum & 1) == 1 ? '1' : '0';  // 取最低位，判断是0还是1
-            absNum >>= 1;  // 右移一位
-            index++;
+        int num = -10;  // 负整数
+        for (int i = 31; i >= 0; i--) {  // 从高位到低位遍历32位二进制数
+            int bit = (num >> i) & 1;  // 取出当前位的值
+            System.out.print(bit);  // 输出当前位的值
         }
-
-//        if (num < 0) {
-            // 负数的补码等于其绝对值的按位取反再加一
-            for (int i = 0; i < binaryArray.length; i++) {
-                binaryArray[i] = binaryArray[i] == '0' ? '1' : '0';
-            }
-            int carry = 1;
-            for (int i = binaryArray.length - 1; i >= 0; i--) {
-                int sum = (binaryArray[i] - '0') + carry;
-                if (sum == 2) {
-                    binaryArray[i] = '0';
-                    carry = 1;
-                } else {
-                    binaryArray[i] = (char) (sum + '0');
-                    carry = 0;
-                }
-            }
-//        }
-
-        String binaryString = new String(binaryArray);
-        System.out.println(binaryString);
     }
 }
