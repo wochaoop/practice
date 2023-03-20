e8387da06046bcbdf8118ff224d80e522c6c331b
IharaYuki <ihraryuki@qq.com>
2023-03-20 15:22:49 +0800

    2023/3/20 课时作业

---
+++ src/com/example/m5/Circle.java
@@ -0,0 +1,19 @@
+package com.example.m5;
+
+public class Circle {
+    private float fRadius;
+    final float PI=3.14f;
+    void setRadius(float fR) {
+        fRadius=fR;
+    }
+    void showArea(){
+        System.out.println("The area of the circle is "+fRadius*fRadius*PI);
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5);
+        circle.showArea();
+    }
+}
+
---
+++ src/com/example/m5/TestStudent.java
@@ -0,0 +1,10 @@
+package com.example.m5;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student xiaoming = new Student();
+        xiaoming.age = 18;
+        xiaoming.name = "王小明";
+        xiaoming.eat();
+    }
+}
