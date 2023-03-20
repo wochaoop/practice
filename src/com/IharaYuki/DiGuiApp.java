12f17fb8b1244778a7b01fd651ee894741349800
IharaYuki <ihraryuki@qq.com>
2023-03-20 15:22:49 +0800

    2023/3/20 课时作业

--- src/com/IharaYuki/DiGuiApp.java
+++ src/com/IharaYuki/DiGuiApp.java
@@ -1,2 +1,10 @@
-package com.IharaYuki;public class DiGuiApp {
+package com.IharaYuki;
+
+public class DiGuiApp {
+    public static void main(String[] args) {
+        System.out.println(fun(4));
+    }
+    static int fun(int n){
+        return (n == 1)? 1 : n*fun(n-1);
+    }
 }
--- src/com/IharaYuki/PrintApp.java
+++ src/com/IharaYuki/PrintApp.java
@@ -1,2 +1,7 @@
-package com.IharaYuki;public class PrintApp {
+package com.IharaYuki;
+
+public class PrintApp {
+    public static void main(String[] args) {
+
+    }
 }
