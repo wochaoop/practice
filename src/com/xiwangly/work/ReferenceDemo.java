84046ecbfac4cd948d7a7d06e2ff31fb1322dc92
xiwangly2 <1334850101@qq.com>
2023-03-27 15:38:54 +0800

    1111

---
+++ src/com/xiwangly/work/ReferenceDemo.java
@@ -0,0 +1,29 @@
+package src.com.xiwangly.work;
+
+class Timer {
+    int minute, second;
+    public Timer(int m, int s) {
+        minute = m;
+        second = s;
+    }
+    void showTime () {
+        System.out.println("现在时间是："+minute+"分"+second+"秒");
+    }
+    static void swapTime (Timer t1, Timer t2) {
+        Timer t = t1;
+        t1 = t2;
+        t2 = t;
+    }
+}
+public class ReferenceDemo {
+    public static void main(String[] args) {
+        Timer t1 = new Timer(9, 10);
+        Timer t2 = new Timer(11, 12);
+        t1.showTime();
+        t2.showTime();
+        System.out.println("使用 swapTime 方法交换 Timer 实例后：");
+        Timer.swapTime(t1, t2);
+        t1.showTime();
+        t2.showTime();
+    }
+}
