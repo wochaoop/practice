d6c0146752e54e774b528ff542428f37d8e28dca
xiwangly2 <1334850101@qq.com>
2023-03-28 09:24:46 +0800

    修改后

--- src/com/xiwangly/work/ReferenceDemo.java
+++ src/com/xiwangly/work/ReferenceDemo.java
@@ -9,11 +9,20 @@ class Timer {
     void showTime () {
         System.out.println("现在时间是："+minute+"分"+second+"秒");
     }
-    static void swapTime (Timer t1, Timer t2) {
-        Timer t = t1;
-        t1 = t2;
-        t2 = t;
+//    static void swapTime (Timer t1, Timer t2) {
+//        Timer t = t1;
+//        t1 = t2;
+//        t2 = t;
+//    }
+    static void swapTime(Timer t1, Timer t2) {
+        int tempMinute = t1.minute;
+        int tempSecond = t1.second;
+        t1.minute = t2.minute;
+        t1.second = t2.second;
+        t2.minute = tempMinute;
+        t2.second = tempSecond;
     }
+
 }
 public class ReferenceDemo {
     public static void main(String[] args) {
