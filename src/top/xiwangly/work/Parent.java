4029a7c411c199f6899774443cf7dedadf970ae2
Xiwangly <1334850101@qq.com>
2023-04-10 14:02:53 +0800

    PPT上的作业

---
+++ src/top/xiwangly/work/Parent.java
@@ -0,0 +1,19 @@
+/*
+定义一个Parent类，有一个属性name，及输出属性方法showInfo；
+定义一个Children类继承于Parent类，Children类有一个属性age，
+在Children类main函数中创建Children类对象，
+该对象调用showInfo方法，观察运行结果。
+*/
+package top.xiwangly.work;
+public class Parent {
+    private String name;
+
+    public Parent(String name) {
+        this.name = name;
+    }
+
+    public void showInfo() {
+        System.out.println("Name: " + this.name);
+    }
+}
+
