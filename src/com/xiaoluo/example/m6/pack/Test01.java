653e9db1aed872b492a35dfca5582e0217f65dec
Max <3428237080@qq.com>
2023-05-11 11:44:40 +0800

    2023/5/9 课时作业

--- src/com/xiaoluo/example/m6/pack/Test01.java
+++ src/com/xiaoluo/example/m6/pack/Test01.java
@@ -3,5 +3,7 @@ package com.xiaoluo.example.m6.pack;
 public class Test01 {
     public static void main(String[] args) {
         OuterClass01 outerClass01 = new OuterClass01();
+        OuterClass01.InerClass01 inerClass01 = outerClass01.new InerClass01();
+        inerClass01.print();
     }
 }
---
+++ src/com/xiaoluo/example/m6/pack/Test12.java
@@ -0,0 +1,8 @@
+package com.xiaoluo.example.m6.pack;
+
+public class Test12 {
+    public static void main(String[] args) {
+        OutClass12 outClass12 = new OutClass12();
+        System.out.println(outClass12.getObject());
+    }
+}
