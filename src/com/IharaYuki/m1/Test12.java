f944388f18f2e338c0cbbcd712eb453bf0c5a5bd
Kagrra_Shion <ihara_yuki@outlook.com>
2023-05-16 14:31:13 +0800

    5 16

---
+++ src/com/IharaYuki/m1/Test12.java
@@ -0,0 +1,9 @@
+package com.IharaYuki.m1;
+
+public class Test12 {
+    public static void main(String[] args) {
+        OutClass12 outClass12 = new OutClass12();
+        System.out.println(outClass12.getObject());
+
+    }
+}
