09beb17fd0ed5d9041b9ba7da4e12dd81be1bb33
xiwangly2 <1334850101@qq.com>
2023-06-05 15:43:08 +0800

    1

--- src/com/PracticalTraining/N1/Task2.java
+++ src/com/PracticalTraining/N1/Task2.java
@@ -1,4 +1,4 @@
-package com.PracticalTraining;
+package com.PracticalTraining.N1;
 
 public class Task2 {
     public static void main(String[] args) {
---
+++ src/com/PracticalTraining/N2/Task1.java
@@ -0,0 +1,4 @@
+package com.PracticalTraining.N2;
+
+public class Task1 {
+}
