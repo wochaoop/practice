91587d923fc0fd74f6875fe4d238ca41c631f028
xiwangly2 <1334850101@qq.om>
2023-06-08 17:04:05 +0800

    N6优化构建工件，增加了断线重连的支持

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ src 中 [example.com](./src/com/example/) 就是课上的手敲练习
 src 中 [exam](./src/com/exam/) 就是考试的内容
 
 src 中 [PracticalTraining](./src/com/PracticalTraining/) 就是实训练习
+
+[Bytecode-Viewer-2.11.2.jar](https://github.com/Konloch/bytecode-viewer)反编译工具
--- src/com/PracticalTraining/N6/Client.java
+++ src/com/PracticalTraining/N6/Client.java
@@ -8,47 +8,53 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 8080;
+
     public static void main(String[] args) {
-        String serverAddress = "localhost";
-        int serverPort = 8080;
-
-        try {
-            // 连接服务器
-            Socket socket = new Socket(serverAddress, serverPort);
-            if (socket.isConnected()) {
-                System.out.println("成功连接到服务器...");
-            }
-            // 获取输入流和输出流
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-
-            // 创建一个线程用于接收服务器消息并输出
-            Thread serverThread = new Thread(() -> {
+        while (true) {
+            try {
+                connectToServer();
+                break; // 如果连接成功，则跳出循环
+            } catch (IOException e) {
+                System.out.println("无法连接到服务器，尝试重新连接...");
                 try {
-                    String serverMessage;
-                    while ((serverMessage = serverReader.readLine()) != null) {
-                        System.out.println("收到服务器消息: " + serverMessage);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    Thread.sleep(3000); // 休眠3秒后尝试重新连接
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
                 }
-            });
-            serverThread.start();
-
-            // 获取用户输入并发送给服务器
-            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
-            String userInput;
-            while ((userInput = userInputReader.readLine()) != null) {
-                serverWriter.println(userInput);
-                System.out.println("已发送消息给服务器: " + userInput);
             }
-
-            // 关闭连接
-            socket.close();
-        } catch (IOException e) {
-            // 抛出异常消息
-            // e.printStackTrace();
-            System.out.println("连接出问题了……");
         }
+    }
+
+    private static void connectToServer() throws IOException {
+        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        if (socket.isConnected()) {
+            System.out.println("成功连接到服务器...");
+        }
+
+        BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                String serverMessage;
+                while ((serverMessage = serverReader.readLine()) != null) {
+                    System.out.println("收到服务器消息: " + serverMessage);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        serverThread.start();
+
+        BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+        while ((userInput = userInputReader.readLine()) != null) {
+            serverWriter.println(userInput);
+            System.out.println("已发送消息给服务器: " + userInput);
+        }
+
+        socket.close();
     }
 }
