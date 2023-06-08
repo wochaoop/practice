dca11798dda4ff08e63f238f738c2601b9e8012d
xiwangly2 <1334850101@qq.com>
2023-06-08 17:04:05 +0800

    N6优化构建工件，增加了断线重连的支持

--- src/com/PracticalTraining/N6/Server.java
+++ src/com/PracticalTraining/N6/Server.java
@@ -9,54 +9,90 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private static final int PORT = 8080;
+    private static final int BACKLOG = 50;
+
     public static void main(String[] args) {
-        int port = 8080;
-        int backlog = 50; // 最大排队等待连接的客户端数
+        while (true) {
+            try {
+                startServer();
+            } catch (IOException e) {
+                System.out.println("服务器错误: " + e.getMessage());
+                try {
+                    Thread.sleep(3000); // 休眠3秒后尝试重新连接
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void startServer() throws IOException {
+        ServerSocket serverSocket = null;
 
         try {
-            // 创建ServerSocket对象，并设置队列长度
-            ServerSocket serverSocket = new ServerSocket(port, backlog);
+            serverSocket = new ServerSocket(PORT, BACKLOG);
             System.out.println("服务器已启动，等待客户端连接...");
 
             while (true) {
-                // 等待客户端连接
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("客户端连接成功，处理客户端请求...");
+                Socket clientSocket = null;
 
-                // 获取输入流和输出流
-                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+                try {
+                    clientSocket = serverSocket.accept();
+                    System.out.println("客户端连接成功，处理客户端请求...");
 
-                // 处理客户端消息并输出
-                Thread inputThread = new Thread(() -> {
-                    try {
-                        String clientMessage;
-                        while ((clientMessage = reader.readLine()) != null) {
-                            System.out.println("客户端消息: " + clientMessage);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            clientSocket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    ClientHandler clientHandler = new ClientHandler(clientSocket);
+                    Thread clientThread = new Thread(clientHandler);
+                    clientThread.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    if (clientSocket != null) {
+                        clientSocket.close();
                     }
-                });
-                inputThread.start();
-
-                // 持续监听客户端的输入
-                BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
-                String userInput;
-                while ((userInput = userInputReader.readLine()) != null) {
-                    // 向客户端发送用户输入的消息
-                    writer.println(userInput);
-                    System.out.println("已发送消息给客户端: " + userInput);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+
+                String clientMessage;
+                while ((clientMessage = reader.readLine()) != null) {
+                    System.out.println("客户端消息: " + clientMessage);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                    if (writer != null) {
+                        writer.close();
+                    }
+                    clientSocket.close();
+                    System.out.println("客户端断开连接");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
