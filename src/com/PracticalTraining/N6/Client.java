89d4f1f0b456ab51df241e0833e2b2df000957ac
xiwangly2 <1334850101@qq.om>
2023-06-08 11:21:47 +0800

    更新了socket，修复多条消息卡住

--- src/com/PracticalTraining/N6/Client.java
+++ src/com/PracticalTraining/N6/Client.java
@@ -9,42 +9,40 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        String serverHost = "localhost";
+        String serverAddress = "localhost";
         int serverPort = 8080;
 
         try {
-            // 连接到服务器
-            Socket socket = new Socket(serverHost, serverPort);
+            // 连接服务器
+            Socket socket = new Socket(serverAddress, serverPort);
             System.out.println("成功连接到服务器...");
 
             // 获取输入流和输出流
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
-            // 不断获取用户输入，并发送给服务器
+            // 创建一个线程用于接收服务器消息并输出
+            Thread serverThread = new Thread(() -> {
+                try {
+                    String serverMessage;
+                    while ((serverMessage = serverReader.readLine()) != null) {
+                        System.out.println("收到服务器消息: " + serverMessage);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            serverThread.start();
+
+            // 获取用户输入并发送给服务器
             BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
             String userInput;
-            while (true) {
-                System.out.print("请输入消息 (输入 'exit' 退出): ");
-                userInput = userInputReader.readLine();
-
-                if (userInput.equalsIgnoreCase("exit")) {
-                    break;
-                }
-
-                // 向服务器发送用户输入的消息
-                writer.println(userInput);
+            while ((userInput = userInputReader.readLine()) != null) {
+                serverWriter.println(userInput);
                 System.out.println("已发送消息给服务器: " + userInput);
-
-                // 读取服务器的响应
-                String response = reader.readLine();
-                System.out.println("服务器的响应: " + response);
             }
 
-            // 关闭资源
-            writer.close();
-            reader.close();
-            userInputReader.close();
+            // 关闭连接
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
