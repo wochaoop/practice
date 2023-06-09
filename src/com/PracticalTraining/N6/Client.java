cd8406c82bd07d9b8928936829f534de1e630275
xiwangly2 <1334850101@qq.om>
2023-06-09 08:15:29 +0800

    N6提交有问题的版本

--- src/com/PracticalTraining/N6/Client.java
+++ src/com/PracticalTraining/N6/Client.java
@@ -1,13 +1,12 @@
 package com.PracticalTraining.N6;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Client {
     private static final String DEFAULT_SERVER_ADDRESS = "localhost";
@@ -40,14 +39,9 @@ public class Client {
             String arg = args[0].trim();
             if (!arg.isEmpty()) {
                 try {
-                    Pattern pattern = Pattern.compile("^(.*?):(\\d+)$");
-                    Matcher matcher = pattern.matcher(arg);
-                    if (matcher.matches()) {
-                        serverAddress = matcher.group(1);
-                        serverPort = Integer.parseInt(matcher.group(2));
-                    } else {
-                        System.out.println("无效的参数格式，使用默认值");
-                    }
+                    String[] parts = arg.split(":");
+                    serverAddress = parts[0];
+                    serverPort = Integer.parseInt(parts[1]);
                 } catch (NumberFormatException e) {
                     System.out.println("无效的端口号参数，使用默认值");
                 }
@@ -64,6 +58,13 @@ public class Client {
         BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
+        // 从当前目录的user.json文件读取用户配置
+        String userConfigPath = System.getProperty("user.dir") + "./user.json";
+        String credentialsJson = readUserConfig(userConfigPath);
+
+        // 将用户凭据作为头部发送给服务器
+        serverWriter.println(credentialsJson);
+
         Thread serverThread = new Thread(() -> {
             try {
                 String serverMessage;
@@ -79,10 +80,23 @@ public class Client {
         BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
         String userInput;
         while ((userInput = userInputReader.readLine()) != null) {
-            serverWriter.println(userInput);
-            System.out.println("已发送消息给服务器: " + userInput);
+            // 在消息中加上用户名前缀
+            String message = username + ": " + userInput;
+            serverWriter.println(message);
+            System.out.println("已发送消息给服务器: " + message);
         }
 
         socket.close();
+    }
+
+    private static String readUserConfig(String configPath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(configPath))) {
+            StringBuilder jsonString = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            return jsonString.toString();
+        }
     }
 }
---
+++ src/top/xiwangly/test/W1.java
@@ -0,0 +1,8 @@
+package top.xiwangly.test;
+
+public class W1 {
+    public static void main(String[] args) {
+        String currentPath = System.getProperty("user.dir");
+        System.out.println("当前运行路径：" + currentPath);
+    }
+}
