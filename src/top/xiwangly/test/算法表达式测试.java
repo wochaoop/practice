2ad5600bd593a5903685806cb100e4c425686c76
xiwangly2 <1334850101@qq.om>
2023-07-18 10:24:54 +0800

    测试

---
+++ src/top/xiwangly/test/算法表达式测试.java
@@ -0,0 +1,16 @@
+package top.xiwangly.test;
+
+public class 算法表达式测试 {
+    public static void main(String[] args) {
+        int n = 4;
+        while (n!=0){
+            System.out.println(n);
+            n--;
+        }
+        System.out.println("==================");
+        int n2 = 4;
+        while (n2 -- > 0){
+            System.out.println(n2);
+        }
+    }
+}
